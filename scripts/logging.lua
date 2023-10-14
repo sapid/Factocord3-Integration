0edb2b850d8d13d5fd9ebf30d5ed6920c8555517
Will Crawford <378681+sapid@users.noreply.github.com>
2023-10-14 02:05:30 +0000

    Changed logging.lua to work with Factocord3

--- scripts/logging.lua
+++ scripts/logging.lua
@@ -1,58 +1,16 @@
-local function on_pre_player_died (e)
-	if e.cause and e.cause.type == "character" then --PvP death
-		print("JLOGGER: DIED: PLAYER: " .. game.get_player(e.player_index).name .. " " .. (game.get_player(e.cause.player.index).name or "no-cause"))
-	elseif (e.cause) then
-		print ("JLOGGER: DIED: " .. game.get_player(e.player_index).name .. " " .. (e.cause.name or "no-cause"))
-	else
-		print ("JLOGGER: DIED: " .. game.get_player(e.player_index).name .. " " .. "no-cause") --e.g. poison damage
-	end
+-- This file is heavily adapted from https://github.com/maxsupermanhd/FactoCord-3.0/blob/master/control.lua and https://github.com/maxsupermanhd/FactoCord-3.0/blob/master/control.lua
+local function PrintToDiscord(msg)
+	localised_print({"", "0000-00-00 00:00:00 [DISCORD] ", msg})
 end
 
--- Determines and logs a leave reason for a player leaving, logs it to script-output/ext/awflogging.out
-local function on_player_left_game(e)
-	local player = game.get_player(e.player_index)
-	local reason
-	if e.reason == defines.disconnect_reason.quit then
-		reason = "quit"
-	elseif e.reason == defines.disconnect_reason.dropped then
-		reason = "dropped"
-	elseif e.reason == defines.disconnect_reason.reconnect then
-		reason = "reconnect"
-	elseif e.reason == defines.disconnect_reason.wrong_input then
-		reason = "wrong_input"
-	elseif e.reason == defines.disconnect_reason.desync_limit_reached then
-		reason = "desync_limit_reached"
-	elseif e.reason == defines.disconnect_reason.cannot_keep_up then
-		reason = "cannot_keep_up"
-	elseif e.reason == defines.disconnect_reason.afk then
-		reason = "afk"
-	elseif e.reason == defines.disconnect_reason.kicked then
-		reason = "kicked"
-	elseif e.reason == defines.disconnect_reason.kicked_and_deleted then
-		reason = "kicked_and_deleted"
-	elseif e.reason == defines.disconnect_reason.banned then
-		reason = "banned"
-	elseif e.reason == defines.disconnect_reason.switching_servers then
-		reason = "switching_servers"
+local function on_player_joined_game(event)
+	local p = game.players[event.player_index];
+	PrintToDiscord(":wave: **" .. p.name .. "** joined.");
+	if(p.admin == true) then
+		p.print(":wave: Welcome admin " .. p.name .. " to server!");
 	else
-		reason = "other"
+		p.print(":wave: Welcome " .. p.name .. " to server!");
 	end
-	game.write_file("ext/awflogging.out", game.table_to_json(
-		{
-			type='leave',
-			playerName=player.name,
-			reason=reason
-		}
-	) .. "\n", true, 0)
-end
-local function on_player_joined_game(e)
-	local player = game.get_player(e.player_index)
-	game.write_file("ext/awflogging.out", game.table_to_json(
-		{
-			type='join',
-			playerName=player.name
-		}
-	) .. "\n", true, 0)
 end
 
 local function get_infinite_research_name(name)
@@ -62,74 +20,125 @@ end
 
 local function on_research_finished(event)
 	local research_name = get_infinite_research_name(event.research.name)
-	print ("JLOGGER: RESEARCH FINISHED: " .. research_name .. " " .. (event.research.level or "no-level"))
+	PrintToDiscord(":science_pack: Research finished: " .. research_name .. " " .. (event.research.level or ""))
 end
 
-local function on_built_entity(event)
-	-- get the corresponding data
-	local player = game.get_player(event.player_index)
-	local data = global.playerstats[player.name]
-	if data == nil then
-		-- format of array: {entities placed, ticks played}
-		global.playerstats[player.name] = {1, 0}
+local function on_player_left_game(e)
+	local p = game.players[e.player_index];
+	local reason
+	if event.reason == defines.disconnect_reason.quit then
+		reason = "quit"
+	elseif event.reason == defines.disconnect_reason.dropped then
+		reason = "dropped"
+	elseif event.reason == defines.disconnect_reason.reconnect then
+		reason = "reconnect"
+	elseif event.reason == defines.disconnect_reason.wrong_input then
+		reason = "wrong_input"
+	elseif event.reason == defines.disconnect_reason.desync_limit_reached then
+		reason = "desync_limit_reached"
+	elseif event.reason == defines.disconnect_reason.cannot_keep_up then
+		reason = "cannot_keep_up"
+	elseif event.reason == defines.disconnect_reason.afk then
+		reason = "afk"
+	elseif event.reason == defines.disconnect_reason.kicked then
+		reason = "kicked"
+	elseif event.reason == defines.disconnect_reason.kicked_and_deleted then
+		reason = "kicked_and_deleted"
+	elseif event.reason == defines.disconnect_reason.banned then
+		reason = "banned"
+	elseif event.reason == defines.disconnect_reason.switching_servers then
+		reason = "switching_servers"
+	end
+	if reason then
+		PrintToDiscord(":wave: **" .. p.name .. "** left (" .. reason .. ").")
 	else
-		data[1] = data[1] + 1 --indexes start with 1 in lua
-		global.playerstats[player.name] = data
+		PrintToDiscord(":wave: **" .. p.name .. "** left.");
 	end
 end
 
-local function on_init ()
-	global.playerstats = {}
-end
-
-local function logStats()
-	-- log built entities and playtime of players
-	for _, p in pairs(game.players)
-	do
-		local pdat = global.playerstats[p.name]
-		if (pdat == nil) then
-				-- format of array: {entities placed, ticks played}
-				pdat = {0, p.online_time}
-				print ("JLOGGER: STATS: " .. p.name .. " " .. 0 .. " " .. p.online_time)
-				global.playerstats[p.name] = pdat
-		else
-			if (pdat[1] ~= 0 or (p.online_time - pdat[2]) ~= 0) then
-				print ("JLOGGER: STATS: " .. p.name .. " " .. pdat[1] .. " " .. (p.online_time - pdat[2]))
+local function on_console_chat(e)
+		if not e.player_index then
+			return
+		end
+		if game.players[e.player_index].tag == "" then
+			if game.players[e.player_index].admin then
+				PrintToDiscord('(Admin) <' .. game.players[e.player_index].name .. '> ' .. e.message)
+			else
+				PrintToDiscord('<' .. game.players[e.player_index].name .. '> ' .. e.message)
 			end
-			-- update the data
-			global.playerstats[p.name] = {0, p.online_time}
+		else
+			if game.players[e.player_index].admin then
+				PrintToDiscord('(Admin) <' .. game.players[e.player_index].name .. '> ' .. game.players[e.player_index].tag .. " " .. e.message)
+			else
+				PrintToDiscord('<' .. game.players[e.player_index].name .. '> ' .. game.players[e.player_index].tag .. " " .. e.message)
+			end
 		end
 	end
+
+
+
+local function on_player_died(event)
+	local p = game.players[event.player_index];
+	local c = event.cause
+	if not c then
+		PrintToDiscord(":skull: **" .. p.name .. "** died.");
+	else
+		local name = "Unknown";
+		if c.type == "character" then
+			name = c.player.name;
+		elseif c.type == "spider-vehicle" then
+			if c.entity_label then
+				name = {"", c.localised_name, " " , c.entity_label};
+			else
+				name = {"", "a ", c.localised_name};
+			end
+		elseif c.type == "locomotive" then
+			name = {"", c.localised_name, " " , c.backer_name};
+		else
+			name = {"", "a ", c.localised_name};
+		end
+		PrintToDiscord({":skull: ", "**", p.name, "** killed by ", name, "."});
+	end
+end
+local function on_player_kicked(event)
+	local p = game.players[event.player_index];
+	PrintToDiscord(":warning: **" .. p.name .. "** kicked.");
+end
+local function on_player_unbanned(event)
+	PrintToDiscord(":warning: **" .. event.player_name .. "** unbanned.");
+end
+local function on_player_unmuted(event)
+	local p = game.players[event.player_index];
+	PrintToDiscord(":warning: **" .. p.name .. "** unmuted.");
+end
+local function on_player_banned(event)
+	PrintToDiscord(":warning: **" .. event.player_name .. "** banned.");
+end
+local function on_player_muted(event)
+	local p = game.players[event.player_index];
+	PrintToDiscord(":warning: **" .. p.name .. "** muted.");
 end
 
-local function on_rocket_launched(e)
-	print ("JLOGGER: ROCKET: " .. "ROCKET LAUNCHED")
-end
 local function checkEvolution(e)
-	print("JLOGGER: EVOLUTION: " .. string.format("%.4f", game.forces["enemy"].evolution_factor))
-end
-local function on_trigger_fired_artillery(e)
-	print ("JLOGGER: ARTILLERY: " .. e.entity.name .. (e.source.name or "no source"))
+	PrintToDiscord(":biohazard: Evolution: " .. string.format("%.4f", game.forces["enemy"].evolution_factor))
 end
 
 local logging = {}
 logging.events = {
-	[defines.events.on_rocket_launched] = on_rocket_launched,
 	[defines.events.on_research_finished] = on_research_finished,
 	[defines.events.on_player_joined_game] = on_player_joined_game,
 	[defines.events.on_player_left_game] = on_player_left_game,
-	[defines.events.on_pre_player_died] = on_pre_player_died,
-	[defines.events.on_built_entity] = on_built_entity,
-	[defines.events.on_trigger_fired_artillery] = on_trigger_fired_artillery,
+	[defines.events.on_player_died] = on_player_died,
+	[defines.events.on_player_kicked] = on_player_kicked,
+	[defines.events.on_player_muted] = on_player_muted,
+	[defines.events.on_player_unmuted] = on_player_unmuted,
+	[defines.events.on_player_banned] = on_player_banned,
+	[defines.events.on_player_unbanned] = on_player_unbanned,
+	[defines.events.on_console_chat] = on_console_chat,
 }
 
 logging.on_nth_tick = {
-	[60*60*15] = function() -- every 15 minutes
-		logStats()
-	end,
-	[60*60] = checkEvolution,
+	[60*60*15] = checkEvolution, -- Every 15 minutes
 }
-
-logging.on_init = on_init
 
 return logging
